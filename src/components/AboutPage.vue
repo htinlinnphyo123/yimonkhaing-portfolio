b89fa3cc39d827c59f155f7da6344953b2da748d
Htin Linn Phyo <htinlinnphyo.npt@gmail.com>
2024-04-17 16:40:34 +0630

    FInal Deploy

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -10,20 +10,22 @@
   </h1>
   <div class="lg:grid lg:grid-cols-2 px-4 md:px-10 lg:px-16  mb-20  max-w-screen overflow-x-hidden">
     <div class="flex justify-center items-center lg:tracking-wide lg:leading-10 mb-8 lg:mb-0 about">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      Since childhood, I've harbored a fervent dream of studying management. During the difficulties of Covid-19 and the Myanmar coup, I had to explore new paths for my future education and discovered my passion for hospitality. With my father's unwavering support and shared aspirations, I confidently selected Bangkok University for its inviting campus, modern facilities, and promising post-graduation opportunities. Excited to dive into the lively BU community, I'm ready to begin my journey in hospitality management with enthusiasm.
     </div>
-    <div class="flex justify-center items-center flex-col hobby">
-      <div class="my-4 lg:my-6 ps-4 border-l-8 border-blue-600">
-        <h2 class="text-lg lg:text-2xl font-bold">Hobby</h2>
-        <p class="text-md lg:text-lg italic">Watching Movie , Listening music , Dancing</p>
+    <div class="hobby lg:px-10">
+      <div class="my-4 lg:my-6 ps-4 border-l-4 border-blue-600">
+        <h2 class="text-lg lg:text-xl font-bold">Hobbies</h2>
+        <p class="text-md lg:text-lg italic">Listening to Music, Sketching, watching productive vlogs</p>
       </div>
-      <div class="my-4 lg:my-6 ps-4 border-l-8 border-red-600">
-        <h2 class="text-lg lg:text-2xl font-bold">Hobby</h2>
-        <p class="text-md lg:text-lg italic">Watching Movie , Listening music , Dancing</p>
+      <div class="my-4 lg:my-6 ps-4 border-l-4 border-red-600">
+        <h2 class="text-lg lg:text-xl font-bold">Ambition</h2>
+        <p class="text-md lg:text-lg italic">To be a hotel manager </p>
       </div>
-      <div class="my-4 lg:my-6 ps-4 border-l-8 border-yellow-600">
-        <h2 class="text-lg lg:text-2xl font-bold">Hobby</h2>
-        <p class="text-md lg:text-lg italic">Watching Movie , Listening music , Dancing</p>
+      <div class="my-4 lg:my-6 ps-4 border-l-4 border-yellow-600">
+        <h2 class="text-lg lg:text-xl font-bold">Strong and Weak points</h2>
+        <p class="text-md lg:text-lg italic">
+          <span class="font-semibold">Strong</span> - time management <br/>
+          <span class="font-semibold">Weak</span> - being too detail-oriented and losing sight of the big picture</p>
       </div>
     </div>
   </div>
@@ -49,6 +51,12 @@ onMounted(()=>{
 
 </script>
 <style>
+h2,.hobby p{
+  text-align: start;
+}
+.hobby h2{
+  margin-bottom: 10px;
+}
 .about{
   transform:translate(-300px);
   opacity:0.5;
